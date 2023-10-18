3718e3aea3454117ca4542b491f02fcb5e0b606e
notzree <notzree@gmail.com>
2023-10-18 00:07:23 -0400

    update

--- backend/init_database.py
+++ backend/init_database.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 import requests
 from typing import List
 from db import database
+from qdrant_client.http.models import PointStruct
 load_dotenv()
 
 
 class CourseInfo:
+    id: str
     courseId: str
     courseOfferNumber: int
     termCode: str
@@ -38,10 +40,28 @@ db.createTables()
 # Get ALL courses in the term
 response = requests.get(f"{url}/Courses/{termCode}", headers={
     'accept': 'application/json',
-    'x-api-key': '71BE603A75AE488CB068D7A9D56333A2'
+    'x-api-key': uw_api_key
 })
 courses: List[CourseInfo] = response.json()
-
-print("Adding courses to database...")
-for course in courses:
+filtered_courses = [
+    course for course in courses if course['associatedAcademicGroupCode'] == 'ENG']
+print(f"Adding {len(filtered_courses)} courses to database...")
+points = []
+for index, course in enumerate(filtered_courses):
+    if course['associatedAcademicGroupCode'] != 'ENG':
+        continue
     db.addClass(course)
+    vectors = db.embedding_client.embed_query(course['description'])
+    points.append(PointStruct(
+        id=index, vector=vectors, payload={"course": course['title'], "id": f"{course['courseId']}{course['subjectCode']}{course['catalogNumber']}"}))
+db.addPoints(points)
+
+
+# Now we have to go through every single node in the database and add the relationships!
+results = db.conn.execute("""
+MATCH (n:Class)
+RETURN n
+""")
+while results.has_next():
+    node = results.get_next()
+    # find all similarities
--- backend/util_functions.py
+++ backend/util_functions.py
@@ -1,6 +1,6 @@
-def generate_term_code(year_month_tuple):
-    year, month = year_month_tuple
-    a = 0 if year < 2000 else 1
-    yy = str(year)[-2:]
-    m = str(month).zfill(2)
-    return f"{a}{yy}{m}"
+# def generate_term_code(year_month_tuple):
+#     year, month = year_month_tuple
+#     a = 0 if year < 2000 else 1
+#     yy = str(year)[-2:]
+#     m = str(month).zfill(2)
+#     return f"{a}{yy}{m}"
